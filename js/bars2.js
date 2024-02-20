aff0ae2da300223ca5b7e63d7b79671aca6f8f17
Nishit Grover <grovernt@mail.uc.edu>
2024-02-19 20:45:57 -0500

    second histogram added

--- js/bars2.js
+++ js/bars2.js
@@ -1,59 +1,59 @@
 // set the dimensions and margins of the graph
-const margin = {top: 60, right: 20, bottom: 50, left: 20};
-const width = 490 - margin.left - margin.right;
-const height = 550 - margin.top - margin.bottom;
+const margin1 = {top: 60, right:0, bottom: 50, left: 40};
+const width2 = 470 - margin1.left - margin1.right;
+const height1 = 550 - margin1.top - margin1.bottom;
 
-// append the svg object to the body of the page
-const svg = d3.select("#histogram")
+// append the svg1 object to the body of the page
+const svg1 = d3.select("#histogram2")
   .append("svg")
-    .attr("width", 490)
+    .attr("width", 470)
     .attr("height", 550)
-    .attr("viewBox", "0 0 490 550")
-    // .attr("preserveAspectRatio", "xMinYMin")
+    .attr("viewBox", "0 0 470 550")
+   //  .attr("preserveAspectRatio", "xMinYMin")
   .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin1.left}, ${margin1.top})`);
 
 // parse the Data
 d3.csv("data/national_health_data.csv")
 .then(function(data){
 
-   data = data.filter(d => d.poverty_perc !== -1);
+   data = data.filter(d => d.percent_no_heath_insurance !== -1);
 
    data.forEach(function(d) {
-      d.poverty_perc = +d.poverty_perc;
+      d.percent_no_heath_insurance = +d.percent_no_heath_insurance;
   });
 
 
 // X scale and Axis
 const xScale = d3.scaleLinear()
-  .domain([0,50])
-  .range([0, width]);
-svg
-svg.append('g')
-.attr("transform", `translate(0, ${height})`)
+.domain(d3.extent(data, d => d.percent_no_heath_insurance))
+.range([0, width2]);
+svg1
+svg1.append('g')
+.attr("transform", `translate(0, ${height1})`)
 .call(d3.axisBottom(xScale)
-  .tickValues(d3.range(0, 48, 2)) 
+  .tickValues(d3.range(0,41, 2)) 
   .tickSize(0)
   .tickPadding(8) );
 
 
-  const maxPovertyPerc = d3.max(data, d => d.poverty_perc);
+  const maxPovertyPerc = d3.max(data, d => d.percent_no_heath_insurance);
 
 // Y scale and Axis
 const yScale = d3.scaleLinear()
-    .range([height, 0])
+    .range([height1, 0])
     .domain([0, maxPovertyPerc]);  
 
 
-const yAxis = svg.append('g')
+const yAxis = svg1.append('g')
 
 // set horizontal grid line
 const GridLine = () => d3.axisLeft().scale(yScale);
-svg
+svg1
   .append("g")
     .attr("class", "grid")
   .call(GridLine()
-    .tickSize(-width,0,0)
+    .tickSize(-width2,0,0)
     .tickFormat("")
     .ticks(10)
 );
@@ -87,7 +87,7 @@ const mouseleave = function(d) {
 
 // set the parameters for the histogram
 const histogram = d3.bin()
-  .value(d => d.poverty_perc)
+  .value(d => d.percent_no_heath_insurance)
   .domain(xScale.domain())
   .thresholds(xScale.ticks(50));
 
@@ -95,7 +95,7 @@ const histogram = d3.bin()
 const bins = histogram(data)
 
 // Scale the range of the data in the y domain
-yScale.domain([0, 300]);
+yScale.domain([0, 170]);
 // yScale.domain([0, d3.max(bins, d => d.length)]);
 
 
@@ -104,45 +104,45 @@ yAxis
   .call(d3.axisLeft(yScale).tickSize(0).tickPadding(4))
   .call(d => d.select(".domain").remove());
 
-// append the bar rectangles to the svg element
-svg
+// append the bar rectangles to the svg1 element
+svg1
   .selectAll("rect")
     .data(bins)
   .join("rect")
     .attr("class", "bar")
-    .attr("x", 1)
+   //  .attr("x", 1)
     .attr("transform", d => `translate(${xScale(d.x0)}, ${yScale(d.length)})`)
     .attr("width", d => Math.max(0, xScale(d.x1) - xScale(d.x0) - 1))
-    .attr("height", d => height - yScale(d.length))
+    .attr("height", d => height1 - yScale(d.length))
     .style("fill", "#0072BC")
   .on("mouseover", mouseover)
   .on("mousemove", mousemove)
   .on("mouseleave", mouseleave);
 
 // set title
-svg
+svg1
   .append("text")
     .attr("class", "chart-title")
-    .attr("x", -(margin.left)*0.4)
-    .attr("y", -(margin.top)/1.5)
+    .attr("x", -(margin1.left)*0.4)
+    .attr("y", -(margin1.top)/1.5)
     .attr("text-anchor", "start")
-  .text("Poverty Rate Distribution")
+  .text("People Without Health Insurance Distribution")
 
 // set X axis label
-svg
+svg1
   .append("text")
     .attr("class", "chart-label")
-    .attr("x", width/2)
-    .attr("y", height+margin.bottom/1.7)
+    .attr("x", width2/2)
+    .attr("y", height1+margin1.bottom/1.7)
     .attr("text-anchor", "middle")
-  .text("Poverty Rate (%)");
+  .text("People without Health Insurance (%)");
 
 // set Y axis label
-svg
+svg1
   .append("text")
     .attr("class", "chart-label")
-    .attr("x", -(margin.left)*0.4)
-    .attr("y", -(margin.top/5))
+    .attr("x", -(margin1.left)*0.4)
+    .attr("y", -(margin1.top/5))
     .attr("text-anchor", "start")
   .text("Number of Counties")
 
